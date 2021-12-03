d7d0002964ffe5d31d734abd86f8cbc01796cce0
Matthew Harrigan <mpharrigan@google.com>
2021-12-02 16:28:31 -0800

    Algo Benchmark Library card catalog
    Change-Id: I4ca692cd92ddbee95d0d2aeed5a4027fcd0f2aa4

---
+++ recirq/algo_benchmark_library.py
@@ -0,0 +1,146 @@
+import dataclasses
+from dataclasses import dataclass
+from functools import lru_cache
+from typing import Type, Callable, List, Tuple
+
+import pandas as pd
+
+from cirq_google import ExecutableSpec, QuantumExecutableGroup
+from recirq.fermi_hubbard.xx_model.single_noninteracting import (
+    SingleNoninteractingXxData,
+    SingleNoninteractingXxSpec,
+    get_all_single_noninteracting_xx_executables
+)
+from recirq.otoc.loschmidt.line_topology import (
+    LineTopologyLoschmidtSpec,
+    LineTopologyLoschmidtData,
+    get_all_line_topology_executables,
+)
+from recirq.otoc.loschmidt.tilted_square_lattice import (
+    TiltedSquareLatticeLoschmidtSpec,
+    TiltedSquareLatticeLoschmidtData,
+    get_all_tilted_square_lattice_executables
+)
+
+
+@dataclass
+class AlgoBenchmark:
+    domain: str
+    """The problem domain. Usually a high-level ReCirq module."""
+
+    name: str
+    """The benchmark name. Must be unique within the domain."""
+
+    executable_family: str
+    """A globally unique identifier for this AlgoBenchmark.
+    
+    This should match up with this Benchmark's `spec_class.executable_family`.
+    
+    By convention, the executable family is the fully-qualified leaf-module where the code
+    for this AlgoBenchmark lives.
+    """
+
+    spec_class: Type[ExecutableSpec]
+    """The ExecutableSpec subclass for this AlgoBenchmark."""
+
+    data_class: Type
+    """The class which can contain ETL-ed data for this AlgoBenchmark."""
+
+    gen_func: Callable[[...], QuantumExecutableGroup]
+    """The function that returns a QuantumExecutableGroup for a given Config."""
+
+    configs: List['BenchmarkConfig']
+
+    def as_strings(self):
+        ret = {k: str(v) for k, v in dataclasses.asdict(self).items()}
+        ret['spec_class'] = self.spec_class.__name__
+        ret['data_class'] = self.data_class.__name__
+        ret['gen_func'] = self.gen_func.__name__
+        return ret
+
+
+@dataclass
+class BenchmarkConfig:
+    short_name: str
+    """The short name for this config. Unique within an AlgoBenchmark."""
+
+    full_name: str
+    """A globally unique name for this config."""
+
+    gen_script: str
+    """The script filename that generates the QuantumExecutableGroup for this Config."""
+
+    run_scripts: List[str]
+    """A list of script filenames that execute (or can execute) this config."""
+
+
+BENCHMARKS = [
+    AlgoBenchmark(
+        domain='recirq.otoc',
+        name='loschmidt.tilted_square_lattice',
+        executable_family='recirq.otoc.loschmidt.tilted_square_lattice',
+        spec_class=TiltedSquareLatticeLoschmidtSpec,
+        data_class=TiltedSquareLatticeLoschmidtData,
+        gen_func=get_all_tilted_square_lattice_executables,
+        configs=[
+            BenchmarkConfig(
+                short_name='small-v1',
+                full_name='tilted_square_lattice-small-v1.json.gz',
+                gen_script='gen-small-v1.py',
+                run_scripts=['run-simulator.py']
+            )
+        ]
+    ),
+    AlgoBenchmark(
+        domain='recirq.otoc',
+        name='loschmidt.line_topology',
+        executable_family='recirq.otoc.loschmidt.line_topology',
+        spec_class=LineTopologyLoschmidtSpec,
+        data_class=LineTopologyLoschmidtData,
+        gen_func=get_all_line_topology_executables,
+        configs=[
+            BenchmarkConfig(
+                short_name='small-v1',
+                full_name='line_topology-small-v1.json.gz',
+                gen_script='gen-small-v1.py',
+                run_scripts=['run-simulator.py']
+            )
+        ],
+    ),
+    AlgoBenchmark(
+        domain='recirq.fermi_hubbard',
+        name='xx_model.single_noninteracting',
+        executable_family='recirq.fermi_hubbard.xx_model.single_noninteracting',
+        spec_class=SingleNoninteractingXxSpec,
+        data_class=SingleNoninteractingXxData,
+        gen_func=get_all_single_noninteracting_xx_executables,
+        configs=[
+            BenchmarkConfig(
+                short_name='small-v1',
+                full_name='single_noninteracting-small-v1.json.gz',
+                gen_script='gen-small-v1.py',
+                run_scripts=['run-simulator.py']
+            )
+        ],
+    )
+]
+
+
+@lru_cache()
+def get_all_algo_configs() -> List[Tuple[AlgoBenchmark, BenchmarkConfig]]:
+    ret = []
+    for algo in BENCHMARKS:
+        for config in algo.configs:
+            ret.append((algo, config))
+    return ret
+
+
+def main():
+    df = pd.DataFrame([algo.as_strings() for algo in BENCHMARKS]).set_index('executable_family')
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+    print(df.transpose())
+
+
+if __name__ == '__main__':
+    main()
